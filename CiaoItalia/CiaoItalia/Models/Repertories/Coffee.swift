ac1f7472ffdea242346dca3bab55551f76cf3d2d
Ana Paula Sá <anapaulasapaiva@gmail.com>
2025-04-04 17:12:46 -0300

    feat: add coffee repertories enum

--- CiaoItalia/CiaoItalia/Models/Repertories/Coffee.swift
+++ CiaoItalia/CiaoItalia/Models/Repertories/Coffee.swift
@@ -5,3 +5,36 @@
 //  Created by Ana Paula Sá Barreto Paiva da Cunha on 04/04/25.
 //
 
+enum Coffee:CaseIterable {
+    case coffee
+    case coffeeShop
+    
+    var imageName:String {
+        switch self {
+        case .coffee:
+            return "coffee"
+        case .coffeeShop:
+            return "coffeeShop"
+        }
+    }
+    
+    var portuguese:String {
+        switch self {
+        case .coffee:
+            return "Café"
+        case .coffeeShop:
+            return "Cafeteria"
+        }
+    }
+    var italian:String {
+        switch self {
+        case .coffee:
+            return "Caffè"
+        case .coffeeShop:
+            return "Caffetteria"
+    
+        }
+    }
+}
+
+
--- CiaoItalia/CiaoItalia/Views/RepertoryCardView.swift
+++ CiaoItalia/CiaoItalia/Views/RepertoryCardView.swift
@@ -8,5 +8,35 @@
 import UIKit
 
 class RepertoryCardView: UIView {
-    
+    private let image = UIImageView()
+    private let portuguese = UILabel()
+    private let italian = UILabel()
+    init(frame:CGRect, pt:String, it:String, imageName:String) {
+        super.init(frame: frame)
+        setupView(imageName:imageName)
+        setupLabels(pt:pt, it:it)
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+    }
+    func setupView(imageName:String) {
+        self.image.image = UIImage(named: imageName)
+        self.image.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(self.image)
+    }
+    func setupLabels(pt:String, it:String) {
+        self.portuguese.text = pt
+        self.portuguese.textColor = .blue
+        self.portuguese.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(self.portuguese)
+        self.italian.text = it
+        self.italian.textColor = .blue
+        self.italian.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(self.italian)
+    }
+    func setupConstraints(){
+        
+    }
 }
+
