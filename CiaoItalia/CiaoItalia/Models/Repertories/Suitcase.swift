488b58cb43945b57046820b7e3cb09cc7064ea4c
Alice Barbosa <alicebmenezes@hotmail.com>
2025-04-22 17:15:23 -0300

    FIX: minor adjustments

--- CiaoItalia/CiaoItalia/Models/Repertories/Suitcase.swift
+++ CiaoItalia/CiaoItalia/Models/Repertories/Suitcase.swift
@@ -66,15 +66,15 @@ enum Suitcase:ContentType {
         return [
             .title("Pedindo informação:"),
             .dialogue(DialogueLine(italian: "Ciao! Sei pronto per il viaggio?", translation: "Olá! Você está pronto para a viagem?", type: .answer)),
-            .dialogue(DialogueLine(italian: "Quasi pronto. Puoi aiutarmi a fare un checklist?", translation: "Quase pronto. Você pode me ajudar a fazer um checklist?", type: .answer)),
-            .dialogue(DialogueLine(italian: "Certo! Prima di tutto, hai il passaporto?", translation: "Claro! Primeiro de tudo, você tem o passaporte?", type: .question)),
-            .dialogue(DialogueLine(italian: "Sì, ce l'ho qui.", translation: "Sim, eu tenho aqui.", type: .answer)),
-            .dialogue(DialogueLine(italian: "Perfetto! E il biglietto di viaggio?", translation: "Perfeito! E o bilhete de viagem?", type: .question)),
-            .dialogue(DialogueLine(italian: "Sì, eccolo.", translation: "Sim, aqui está.", type: .answer)),
-            .dialogue(DialogueLine(italian: "Ottimo! Hai una carta?", translation: "Ótimo! Você tem um mapa?", type: .question)),
-            .dialogue(DialogueLine(italian: "Sì, ho una carta.", translation: "Sim, eu tenho um mapa.", type: .answer)),
-            .dialogue(DialogueLine(italian: "Benissimo! Sei pronto per partire. Buon viaggio!", translation: "Perfeito! Você está pronto para partir. Boa viagem!", type: .question)),
-            .dialogue(DialogueLine(italian: "Grazie! Buon viaggio!", translation: "Obrigado! Boa viagem!", type: .answer)),
+            .dialogue(DialogueLine(italian: "Quasi pronto. Puoi aiutarmi a fare un checklist?", translation: "Quase pronto. Você pode me ajudar a fazer um checklist?", type: .question)),
+            .dialogue(DialogueLine(italian: "Certo! Prima di tutto, hai il passaporto?", translation: "Claro! Primeiro de tudo, você tem o passaporte?", type: .answer)),
+            .dialogue(DialogueLine(italian: "Sì, ce l'ho qui.", translation: "Sim, eu tenho aqui.", type: .question)),
+            .dialogue(DialogueLine(italian: "Perfetto! E il biglietto di viaggio?", translation: "Perfeito! E o bilhete de viagem?", type: .answer)),
+            .dialogue(DialogueLine(italian: "Sì, eccolo.", translation: "Sim, aqui está.", type: .question)),
+            .dialogue(DialogueLine(italian: "Ottimo! Hai una carta?", translation: "Ótimo! Você tem um mapa?", type: .answer)),
+            .dialogue(DialogueLine(italian: "Sì, ho una carta.", translation: "Sim, eu tenho um mapa.", type: .question)),
+            .dialogue(DialogueLine(italian: "Benissimo! Sei pronto per partire. Buon viaggio!", translation: "Perfeito! Você está pronto para partir. Boa viagem!", type: .answer)),
+            .dialogue(DialogueLine(italian: "Grazie! Buon viaggio!", translation: "Obrigado! Boa viagem!", type: .question)),
             
                 .end("Fim de diálogo")
         ]
