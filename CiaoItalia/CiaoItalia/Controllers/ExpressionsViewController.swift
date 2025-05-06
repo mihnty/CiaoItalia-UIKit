f2a3110ce4a7d550bcccc4c1d9122a24863e2fa5
fernandesbarreto <fernandespedrobc@gmail.com>
2025-05-06 17:24:09 -0300

    CHORE: working search bar

--- CiaoItalia/CiaoItalia/Controllers/ExpressionsViewController.swift
+++ CiaoItalia/CiaoItalia/Controllers/ExpressionsViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ExpressionsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+class ExpressionsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+    
     var expressions: Expressions
+    var filteredExpressions: [ExpressionInfo]
     
     let titleLabel: UILabel = {
         let label = UILabel()
@@ -19,8 +21,16 @@ class ExpressionsViewController: UIViewController, UITableViewDataSource, UITabl
         return label
     }()
     
+    let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.placeholder = "Pesquisar"
+        return searchBar
+    }()
+    
     init(expressions: Expressions){
         self.expressions = expressions
+        self.filteredExpressions = expressions.expressionList
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -29,11 +39,11 @@ class ExpressionsViewController: UIViewController, UITableViewDataSource, UITabl
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        expressions.expressionList.count
+        return filteredExpressions.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let line = expressions.expressionList[indexPath.row]
+        let line = filteredExpressions[indexPath.row]
         guard let cell = expressionsTableView.dequeueReusableCell(withIdentifier: WordCardCell.identifier, for: indexPath ) as? WordCardCell else {
             return UITableViewCell()
         }
@@ -53,10 +63,11 @@ class ExpressionsViewController: UIViewController, UITableViewDataSource, UITabl
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
-        
     }
-    private func setup(){
+    
+    private func setup() {
         setupTitle()
+        setupSearchBar()
         setupViewHierarchy()
         setupViewAttributes()
         setupConstraints()
@@ -68,8 +79,13 @@ class ExpressionsViewController: UIViewController, UITableViewDataSource, UITabl
         titleLabel.text = expressions.title
     }
     
+    private func setupSearchBar() {
+        searchBar.delegate = self
+    }
+    
     private func setupViewHierarchy() {
         view.addSubview(titleLabel)
+        view.addSubview(searchBar)
         view.addSubview(expressionsTableView)
     }
     
@@ -82,13 +98,34 @@ class ExpressionsViewController: UIViewController, UITableViewDataSource, UITabl
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
             titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
-            expressionsTableView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
+            
+            searchBar.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
+            searchBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            searchBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            
+            expressionsTableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 16),
             expressionsTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             expressionsTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             expressionsTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
         ])
     }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            filteredExpressions = expressions.expressionList
+        } else {
+            filteredExpressions = expressions.expressionList.filter { line in
+                return line.italian.lowercased().contains(searchText.lowercased())
+            }
+        }
+        expressionsTableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
 }
+
 
 #Preview {
     ExpressionsViewController(expressions: FirstWords())
